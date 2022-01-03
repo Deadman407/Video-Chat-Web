17bf9598356ff8c2f60afba4d5dec12ce3a8d4cc
Deadman407 <dead.407.man@gmail.com>
2022-01-03 06:42:15 -0800

    Updated Notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import './App.css';
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import VideoPlayer from './components/VideoPlayer'
-import Options from './components/Options'
-import Notifications from './components/Notifications'
-
-import { Typography, AppBar } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core';
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -32,23 +30,22 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     width: '100%',
   },
-}))
+}));
 
 const App = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={classes.wrapper}>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography variant='h2' align='center'>Video Chat</Typography>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Video Chat</Typography>
       </AppBar>
       <VideoPlayer />
       <Options>
         <Notifications />
       </Options>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
